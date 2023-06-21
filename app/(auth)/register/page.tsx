e015419e2201c1ddcce3ff579564e98d96c3f671
Chris Gambrell <christopher@gambrell.info>
2023-06-21 11:06:21 -0400

    add register page

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+// TODO: There needs to be a page for admins to add accounts
+
 import { Button, buttonVariants } from '@/components/ui/button'
 import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
@@ -13,36 +15,54 @@ import { useForm } from 'react-hook-form'
 import { toast } from 'react-hot-toast'
 import { z } from 'zod'
 
-const formSchema = z.object({ email: z.string().email(), password: z.string().nonempty() })
+const formSchema = z.object({ name: z.string().nonempty(), email: z.string().email(), password: z.string().min(6) })
 type TForm = z.infer<typeof formSchema>
 
-export default function LoginPage() {
-	const form = useForm<TForm>({ resolver: zodResolver(formSchema), defaultValues: { email: '', password: '' } })
+export default function RegisterPage() {
+	const form = useForm<TForm>({ resolver: zodResolver(formSchema), defaultValues: { name: '', email: '', password: '' } })
 	const router = useRouter()
 	const searchParams = useSearchParams()
 
-	async function onSubmit({ email, password }: TForm) {
-		const { error } = await supaclient().auth.signInWithPassword({ email, password })
-		if (error) return toast.error(error.message)
+	async function onSubmit({ name, email, password }: TForm) {
+		const {
+			data: { user },
+			error: signUpError,
+		} = await supaclient().auth.signUp({ email, password })
+		if (signUpError || !user) return toast.error(signUpError?.message || 'Something went wrong. Please try again.')
 
-		router.refresh()
-		router.push(searchParams.get('redirectTo') || '/')
+		const { error: profileError } = await supaclient().from('profiles').insert({ id: user.id, name })
+		if (profileError) return toast.error(profileError.message)
+
+		toast.success('Successfully registered! Please check your email to verify your account.')
+		router.push(`/login?redirectTo=${searchParams.get('redirectTo')}`)
 	}
 
 	return (
 		<div className='container flex flex-col items-center justify-center w-screen h-screen'>
 			<Link
-				href={{ pathname: '/register', query: { redirectTo: searchParams.get('redirectTo') } }}
+				href={{ pathname: '/login', query: { redirectTo: searchParams.get('redirectTo') } }}
 				className={cn(buttonVariants({ variant: 'ghost' }), 'absolute right-4 top-4 md:right-8 md:top-8')}>
-				Register
+				Login
 			</Link>
 			<div className='mx-auto flex w-full flex-col justify-center space-y-4 sm:w-[350px]'>
 				<div className='flex flex-col space-y-1 text-center'>
-					<H3>Welcome back</H3>
-					<Muted>Enter your credentials to sign in</Muted>
+					<H3>Let&apos;s get started</H3>
+					<Muted>Fill out your information below</Muted>
 				</div>
 				<Form {...form}>
 					<form className='space-y-4' onSubmit={form.handleSubmit(onSubmit)}>
+						<FormField
+							control={form.control}
+							name='name'
+							render={({ field }) => (
+								<FormItem>
+									<FormControl>
+										<Input placeholder='Full name' {...field} />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
 						<FormField
 							control={form.control}
 							name='email'
@@ -68,7 +88,7 @@ export default function LoginPage() {
 							)}
 						/>
 						<Button className='w-full' type='submit'>
-							Log in
+							Register
 						</Button>
 					</form>
 				</Form>
--- lib/database.types.ts
+++ lib/database.types.ts
@@ -55,6 +55,31 @@ export interface Database {
           }
         ]
       }
+      profiles: {
+        Row: {
+          created_at: string
+          id: string
+          name: string
+        }
+        Insert: {
+          created_at?: string
+          id: string
+          name: string
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          name?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "profiles_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       streams: {
         Row: {
           created_at: string
