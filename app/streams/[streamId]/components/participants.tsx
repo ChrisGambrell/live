22e04e23a0167f840ad1ac3d0dec69abb0651d52
Chris Gambrell <christopher@gambrell.info>
2023-08-01 11:59:23 -0400

    indicate who is the host

--- app/streams/[streamId]/components/participants.tsx
+++ app/streams/[streamId]/components/participants.tsx
@@ -3,7 +3,6 @@ import { useMeeting } from '@videosdk.live/react-sdk'
 
 export default function Participants() {
 	const { participants } = useMeeting()
-	// console.log([...participants.values()])
 
 	return (
 		<Card className='w-[400px] flex-shrink-0'>
@@ -12,6 +11,11 @@ export default function Participants() {
 				<CardDescription>There are {participants.size - 1} people watching the stream</CardDescription>
 			</CardHeader>
 			<CardContent>
+				{[...participants.values()]
+					.filter((p) => p.mode === 'CONFERENCE')
+					.map((p) => (
+						<p key={p.id}>{p.displayName} (Host)</p>
+					))}
 				{[...participants.values()]
 					.filter((p) => p.mode !== 'CONFERENCE')
 					.map((p) => (
